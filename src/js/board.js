4c2f54ff663d27f8999c06d79139f7f1012ea648
Sun Jung Kim <blackmilk274@gmail.com>
2021-01-17 13:39:18 +0900

    refactor: 큐브 관련 코드 모듈화
    - mesh와 cube 코드 분리
    - board에서 cube를 조작하도록 변경

--- src/js/board.js
+++ src/js/board.js
@@ -3,40 +3,20 @@ import Cube from './cube.js';
 import CustomCamera from './camera.js';
 import CustomScene from './scene.js';
 import CustomRenderer from './renderer.js';
-import CustomMesh from './mesh.js';
-import { CUBE_SIZE } from '../common/constants.js';
 
 const camera = CustomCamera.init();
 const renderer = CustomRenderer.init();
 const scene = CustomScene.init();
-
-// 회전값을 업데이트할 객체들
-// const objects = [];
-// 회전을 관장하는 객체.
-
-const core = new THREE.Object3D(); // 가운데 코어
-const coreY = CustomMesh.createLine(
-  // Y축으로 회전
-  [0, -CUBE_SIZE / 2, 0],
-  [0, CUBE_SIZE / 2, 0],
-); // z축
-const coreZ = CustomMesh.createLine(
-  // Z축으로 회전
-  [0, 0, -CUBE_SIZE / 2],
-  [0, 0, CUBE_SIZE / 2],
-); // z축
-const xyScene = new THREE.Object3D(); // xy평면
-const xzScene = new THREE.Object3D(); // xz평면
+const core = Cube.core;
 
 const render = function (camera, renderer, time) {
   time *= 0.005;
   if (renderer.resizeRenderToDisplaySize()) {
-    // TODO: 아래 두 함수 카메라 안 함수로 묶고, cameara init 리턴 수정
     camera.updateAspect(renderer.getRendererAspect());
   }
   // TODO: 0,0,0을 중심으로 회전하도록 수정
-  coreZ.rotation.z = time;
-  coreY.rotation.y = time;
+  core.center.rotation.z = time;
+  core.yAxis.rotation.y = time;
   renderer.render(scene, camera.getCamera());
 };
 
@@ -47,21 +27,8 @@ const animate = function (camera, renderer) {
 
 // eslint-disable-next-line import/prefer-default-export
 export function init() {
-  renderer.render(scene, camera.getCamera());
-
-  // TODO: line으로부터방향 알아내서 testPlane에 법선으로 적용하기
-  const plane = CustomMesh.createPlane(CUBE_SIZE, CUBE_SIZE, 0x4837ef);
-  plane.translateY(CUBE_SIZE / 2);
-  plane.rotateX(-Math.PI / 2);
-
-  // 중심 축 추가
-  scene.add(core);
-  core.add(coreY);
-  core.add(coreZ);
-  coreZ.add(coreY);
-  coreY.add(plane);
-
-  coreY.material.color.set(0x7aeecc);
-
+  // renderer.render(scene, camera.getCamera());
+  Cube.init();
+  scene.add(core.center);
   animate(camera, renderer);
 }
--- src/js/cube.js
+++ src/js/cube.js
@@ -1,7 +1,37 @@
 // import * as THREE from 'three'; // webpack으로 모듈 사용시
+import CustomMesh from './mesh.js';
 import * as THREE from '../../lib/three.module.js';
+import { CUBE_SIZE } from '../common/constants.js';
+
+const addObject = function (target, obj) {
+  target.add(obj);
+};
 
 // namespace
 const Cube = {};
+
+Cube.core = {
+  center: new THREE.Object3D(),
+  xAxis: CustomMesh.createLine([-CUBE_SIZE, 0, 0], [CUBE_SIZE, 0, 0]),
+  yAxis: CustomMesh.createLine([0, -CUBE_SIZE / 2, 0], [0, CUBE_SIZE / 2, 0]),
+  zAxis: CustomMesh.createLine([0, 0, -CUBE_SIZE / 2], [0, 0, CUBE_SIZE / 2]),
+};
+
+Cube.createPlane = function (color) {
+  return CustomMesh.createPlane(CUBE_SIZE, CUBE_SIZE, color);
+};
+
+// Z -> X -> Y 순으로 회전 (오일러 회전과 순서를 맞춤)
+Cube.init = function () {
+  addObject(this.core.center, this.core.zAxis);
+  addObject(this.core.center, this.core.xAxis);
+  addObject(this.core.center, this.core.yAxis);
+
+  // TODO: line으로부터방향 알아내서 testPlane에 법선으로 적용하기
+  const plane = this.createPlane(0x57838f);
+  plane.translateY(CUBE_SIZE / 2);
+  plane.rotateX(-Math.PI / 2);
+  addObject(this.core.yAxis, plane);
+};
 
 export default Cube;
