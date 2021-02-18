1d41793c83e919f5328ed0f320db06ee14b4b02e
Sun Jung Kim <blackmilk274@gmail.com>
2021-02-18 22:34:32 +0900

    chore: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'import/no-unresolved': 'off',
     'import/extensions': 'off',
     'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'no-underscore-dangle': ['error', { allow: ['foo_', '_bar'] }],
     /* eslint-disable import/extensions */
   },
 };
